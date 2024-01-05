fbfe95f485c665ab6566fd59f2d77413465471f7
G.Chagas <G.Chagas.A@gmail.com>
2024-01-05 10:22:48 -0300

    Adiciona arquivos referente ao curso de NextJs

--- estudos/nextjs-dashboard/app/dashboard/layout.tsx
+++ estudos/nextjs-dashboard/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(
                 <SideNav />
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-                { children }
+                {children}
             </div>
         </div>
     );
